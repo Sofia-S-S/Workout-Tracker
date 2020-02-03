6d9ea0e18a8bf4289485c810b91b21a8e5cd69a9
Sofka <sofka.vs@gmail.com>
2020-02-02 23:18:50 -0600

    first commit

---
+++ Develop/routes/html.js
@@ -0,0 +1,28 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+const router = require("express").Router();
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads index.html
+  //app.get("/", function(req, res) {
+  //res.sendFile(path.join(__dirname, "../public/index.html"));
+  //});
+  //routes for exercise.html
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    console.log(res);
+  });
+  //routes for stats.html
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
