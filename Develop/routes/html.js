ae8b6ff7093bb7200d1757a95f465ef925a15158
Sofka <sofka.vs@gmail.com>
2020-02-05 03:26:33 -0600

    fixed routs

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -9,20 +9,20 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+// Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads index.html
-  //app.get("/", function(req, res) {
-  //res.sendFile(path.join(__dirname, "../public/index.html"));
-  //});
-  //routes for exercise.html
-  app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    console.log(res);
-  });
-  //routes for stats.html
-  app.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-  });
-};
+// index route loads index.html
+//app.get("/", function(req, res) {
+//res.sendFile(path.join(__dirname, "../public/index.html"));
+//});
+//routes for exercise.html
+router.get("/exercise", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  console.log(res);
+});
+//routes for stats.html
+router.get("/stats", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
